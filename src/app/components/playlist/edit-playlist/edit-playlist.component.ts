348992e8ca991ca9030861d0b0b184c408ca0eea
Nguyen Hung <billy7d95@gmail.com>
2020-11-25 21:48:49 +0700

    add edit-playlist component,service,routing

--- src/app/components/playlist/edit-playlist/edit-playlist.component.ts
+++ src/app/components/playlist/edit-playlist/edit-playlist.component.ts
@@ -1,4 +1,6 @@
+import { PlaylistService } from './../../../services/playlist/playlist.service';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-edit-playlist',
@@ -7,9 +9,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditPlaylistComponent implements OnInit {
 
-  constructor() { }
+
+  idPlaylist:number;
+  playlist:any = {
+    name: '',
+    musicType: '',
+    description:''
+  };
+
+
+  constructor(private playlistService:PlaylistService, private actRouter:ActivatedRoute, private router:Router) {
+    this.idPlaylist = parseInt(this.actRouter.snapshot.params.id);
+    this.getPlaylistById(this.idPlaylist)
+   }
 
   ngOnInit(): void {
+  }
+
+  getPlaylistById(id) {
+    this.playlistService.getPlaylistById(id)
+        .then(res => {
+          this.playlist = res;
+
+        }, error => window.alert('errorr'));
+
+  }
+
+  editPlaylist(id){
+    this.playlistService.updatePlaylist(id,this.playlist)
+    .then(res => {
+      this.playlist = res;
+      // this.router.navigateByUrl('/playlists');
+    }, error => window.alert('errorr'));
   }
 
 }
