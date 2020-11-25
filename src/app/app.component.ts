1c2d58f016c204b7497943b2e6d8dcbc100db218
Nguyen Hung <billy7d95@gmail.com>
2020-11-25 20:58:59 +0700

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Case6-FE-master-structure';
+}
